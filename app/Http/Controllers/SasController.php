9d7ef3e2092fab3e7738c5fa448d8cbf49c9f129
AhmadHanifa <97310837+AhmadHanifa@users.noreply.github.com>
2024-08-05 18:44:57 +0200

    fwh

---
+++ app/Http/Controllers/SasController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Functions\POSTFunction;
+use \App\Functions\GETFunction;
+
+use App\Models\Sas;
+
+class SasController extends Controller
+{
+    
+}
---
+++ app/Models/Jahesh.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jahesh extends Model
+{
+      protected $table = 'jahesh';
+    protected $fillable = ['Nummer', 'ewf', 'edcx'];
+    protected $hidden = [];
+
+    
+}
